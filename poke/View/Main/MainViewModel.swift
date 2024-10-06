e72ece744881d871cba6a09437c0b2e13b29720e
bloom <kimrinoffdev@gmail.com>
2024-10-06 14:34:54 +0900

    level 8

--- poke/View/Main/MainViewModel.swift
+++ poke/View/Main/MainViewModel.swift
@@ -7,24 +7,50 @@
 
 import Foundation
 import RxSwift
+import RxCocoa
 
 class MainViewModel {
     private let repository = PokemonRepository()
-    
+    private var currentOffset = 0
+    private let limit = 20
+    private let pokemonListRelay = BehaviorRelay<[Pokemon]>(value: [])
+    private let disposeBag = DisposeBag()
     init() {}
     
     func transform(_ input: Input) -> Output {
-        let pokemonList = input.load
+        let initialLoad = input.load
         // 튜플로 만들어주고
             .withUnretained(self)
         //self 를 owner 라는 이름으로 맨 마지막 이벤트만 받겠다?
             .flatMapLatest { owner, _ in
-                owner.repository.fetchPokemonList(limit: 20, offset: 0)
+                owner.repository.fetchPokemonList(limit: owner.limit, offset: 0)
                 
-            }.map { $0.results }
-            .asObservable()
+            }
+            .do(onNext: { [weak self] response in
+                guard let self = self else { return }
+                self.currentOffset = self.limit
+                self.pokemonListRelay.accept(response.results)
+            })
+        let loadMore = input.loadMore
+            .withUnretained(self)
+            .filter { owner, _ in
+                owner.currentOffset > 0
+            }
+            .flatMapLatest { owner, _ in
+                owner.repository.fetchPokemonList(limit: owner.limit, offset: owner.currentOffset)
+                
+            }
+            .do(onNext: { [weak self]  response in
+                guard let self = self else { return }
+                self.currentOffset += self.limit
+                self.pokemonListRelay.accept(self.pokemonListRelay.value + response.results)
+            })
+                
+        Observable.merge(initialLoad, loadMore)
+            .subscribe()
+            .disposed(by: disposeBag)
         
-        return .init(pokemonList: pokemonList)
+        return .init(pokemonList: pokemonListRelay.asObservable())
     }
     
 }
@@ -33,6 +59,7 @@ extension MainViewModel {
     
     struct Input {
         let load: Observable<Void>
+        let loadMore: Observable<Void>
     }
     
     struct Output {
@@ -40,4 +67,5 @@ extension MainViewModel {
     }
     
 }
+// observer relay subject 의차이점
 
--- poke/View/Main/PokemonCell.swift
+++ poke/View/Main/PokemonCell.swift
@@ -6,11 +6,15 @@
 //
 
 import RxSwift
+import Kingfisher
+
 import UIKit
 
-class PokemonCell: UICollectionViewCell, CellReusable {
+final class PokemonCell: UICollectionViewCell, CellReusable {
     private let imageView = UIImageView()
     private let disposeBag = DisposeBag()
+    
+  
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -28,21 +32,27 @@ class PokemonCell: UICollectionViewCell, CellReusable {
         ])
     }
     
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
+        imageView.kf.cancelDownloadTask()
         imageView.image = nil
+    }
+    func setupUI() {
+        
     }
     
     func configure(with pokemon: Pokemon) {
         if let url = URL(string: pokemon.imageURL) {
-            imageView.rx.loadImage(url: url)
-                .observe(on: MainScheduler.instance)
-                .bind(to: imageView.rx.image)
-                .disposed(by: disposeBag)
+//            imageView.rx.loadImage(url: url)
+//                .observe(on: MainScheduler.instance)
+//                .bind(to: imageView.rx.image)
+//                .disposed(by: disposeBag)
+            imageView.kf.setImage(with: url, placeholder: UIImage(named: "placeholder"))
         }
     }
 }
